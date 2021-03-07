c022ab2d6c7fca3d49a0a1eb5ce1def63e1dda7d
oss5824 <45011625+oss5824@users.noreply.github.com>
2021-03-07 15:11:11 +0900

    ViewFlipper

---
+++ android7/app/src/main/java/com/example/android7/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.android7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ViewFlipper;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnPrev,btnNext;
+        final ViewFlipper vFlipper;
+        btnPrev=findViewById(R.id.btnPrev);
+        btnNext=findViewById(R.id.btnNext);
+        vFlipper=findViewById(R.id.viewFlipper1);
+        btnPrev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                vFlipper.showPrevious();
+            }
+        });
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                vFlipper.showNext();
+            }
+        });
+    }
+}
